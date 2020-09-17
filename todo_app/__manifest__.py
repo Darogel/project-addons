03009019c568c729b84d65eacca25c8541a22e46
darogel <darogel96@gmail.com>
2020-09-17 15:39:33 -0700

    Modificaciones del proyecto todo_app v1

--- todo_app/__manifest__.py
+++ todo_app/__manifest__.py
@@ -4,21 +4,21 @@
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com
-        Prueba de Odoo con Git y Postgress""",
+        Modulo de Prueba del curso de Odoo Full Stack   
+        """,
 
     'description': """
         Long description of module's purpose
         Escuela FullStack Odoo 13
     """,
 
-    'author': "My Company",
+    'author': "Darwin Rogel",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Localizacion',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
