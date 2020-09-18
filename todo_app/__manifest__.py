cdd5b09ea1df9a108d90b469dca88285ed0c3d10
darogel <darogel96@gmail.com>
2020-09-17 18:24:57 -0700

    Creacion del modelo

--- todo_app/__manifest__.py
+++ todo_app/__manifest__.py
@@ -13,7 +13,7 @@
     """,
 
     'author': "Darwin Rogel",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.google.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
