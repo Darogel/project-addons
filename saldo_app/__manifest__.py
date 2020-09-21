f10e74fa8c403d32dfe1880314b599df24c5b703
darogel <darogel96@gmail.com>
2020-09-21 15:51:34 -0700

    Agregando Moneda, Agregando Notificacion

--- saldo_app/__manifest__.py
+++ saldo_app/__manifest__.py
@@ -13,7 +13,7 @@
     "category": 'Ventas',
 
     "version":"1.0",
-    "depends":["base"],
+    "depends":["base", "mail"],
     "data":[
         "security/ir_model_access.xml",
         "views/views.xml"
--- saldo_app/models/models.py
+++ saldo_app/models/models.py
@@ -3,17 +3,20 @@ from odoo import fields, models
 class Movimiento(models.Model):
     _name = "sa.movimiento" #sa_movimiento
     _description = "Movimiento"
+    _inherit = "mail.thread"
 
     name = fields.Char(string="Nombre", required=True)
     type_mov = fields.Selection(selection=[("ingreso","Ingreso"),("gasto","Gasto")], string="Tipo",default="ingreso",required=True)
     date = fields.Datetime(string="Fecha")
-    amount = fields.Float(string="Monto")
+    amount = fields.Float(string="Monto", track_visibility="onchange")
     receipt_image = fields.Binary(string="Foto del recibo")
     notas = fields.Html(string="Notas")
+
     #Relaciones
     user_id = fields.Many2one("res.users", string="Usuario")
     category_id = fields.Many2one("sa.categoria",string="Categoria")
     tag_ids = fields.Many2many("sa.tag","sa_mov_sa_tag_rel","move_id","tag_id")
+    currency_id = fields.Many2one("res.currency", default=2)
 
 class Category(models.Model):
     _name = "sa.categoria"
